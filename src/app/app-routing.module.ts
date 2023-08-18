954143ccf7ff0e6c988c6bb1f98d27cd3a68f867
JakubKonkol <s24406@pjwstk.edu.pl>
2023-08-18 20:18:53 +0200

    visuals

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
-    path: "", component: AppComponent
+    path: "", component: DashboardComponent
   }
 ];
 
