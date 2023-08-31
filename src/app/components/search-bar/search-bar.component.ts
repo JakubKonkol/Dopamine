b3ddc5d38b1d9d7abb5ed3a1f7c6cbc782551567
JakubKonkol <s24406@pjwstk.edu.pl>
2023-08-31 14:39:50 +0200

    redesign part 2

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 import {Router} from "@angular/router";
+import {HotToastService} from "@ngneat/hot-toast";
 
 @Component({
   selector: 'app-search-bar',
@@ -8,11 +9,23 @@ import {Router} from "@angular/router";
 })
 export class SearchBarComponent{
   searchField: string = '';
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router,
+              private toast: HotToastService,
+              private el: ElementRef
+  ) {}
 
   search() {
-    if(this.searchField.length > 0)
-      this.router.navigate(['/search', this.searchField]);
+    if(this.searchField.length < 2) {
+      this.toast.warning('Search query must be at least 2 characters long', {
+        duration: 1500
+      });
+      return;
+    }
+      this.router.navigate(['/search', this.searchField]).then();
+  }
+
+  toggleGlow() {
+    const el = this.el.nativeElement.querySelector('.search-box');
+    el.classList.toggle('glow');
   }
 }
