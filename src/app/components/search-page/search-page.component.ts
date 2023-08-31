8907214b5de97ca541169130e6b5bcf9b26d73fa
JakubKonkol <s24406@pjwstk.edu.pl>
2023-08-31 15:48:46 +0200

    fixed search page

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,29 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {MovieService} from "../../service/MovieService";
 import {Movie} from "../../model/Movie";
 import {HotToastService} from "@ngneat/hot-toast";
 import {toastConfig} from "../../tools/toastConfig";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent implements OnInit{
+export class SearchPageComponent implements OnInit, OnDestroy{
 
-  constructor(private route: ActivatedRoute, private movieService: MovieService, private toast: HotToastService, private router: Router){ }
+  constructor(private route: ActivatedRoute,
+              private movieService: MovieService,
+              private toast: HotToastService,
+              private router: Router,
+  ){ }
   foundMovies: Movie[] = []
   shouldRenderData: boolean = true;
   query: string = "";
+  routeSub: Subscription = new Subscription();
   async ngOnInit(): Promise<void> {
-      this.query = this.route.snapshot.params['query'];
+    this.routeSub = this.route.params.subscribe(params => {
+      this.query = params['query'];
       this.foundMovies = this.movieService.searchMovie(this.query);
-      console.log(this.foundMovies)
-      console.log(this.query)
+    })
 
   }
-
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe(); //memory efficient XD
+  }
     gotoMovie(id: number) {
         this.router.navigate(['/movie', id]).then();
     }
