34b071b3e3f505a4ed78245ffff7bab1d8ee7b91
JakubKonkol <s24406@pjwstk.edu.pl>
2023-10-13 16:15:45 +0200

    update environment.template.ts

--- src/app/environments/environment.template.ts
+++ src/app/environments/environment.template.ts
@@ -1,3 +1,12 @@
 export const environment2 = {
   TMDB_API_KEY: 'API KEY GOES HERE',
+  firebaseConfig: {
+    apiKey: "API KEY GOES HERE",
+    authDomain: "XXX",
+    projectId: "XXX",
+    storageBucket: "XXX",
+    messagingSenderId: "XXX",
+    appId: "XXX",
+    measurementId: "XXX"
+  }
 }
